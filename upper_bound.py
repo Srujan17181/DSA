cd527755d212e3c49f632ba79f82ceb24d6c8d12
Srujan A T <srujanatsruju1718@gmail.com>
2026-03-29 22:54:23 +0530

    Upper Bound

---
+++ upper_bound.py
@@ -0,0 +1,14 @@
+def lowerBound(nums, x):
+    n=len(nums)
+    low=0
+    high=n-1
+    while(low<=high):
+        mid=((low+high)//2)
+        if nums[mid]>x:
+            return mid
+        else:
+            low=mid+1
+    return n
+
+arr=[1,2,2,3]
+print(lowerBound(arr,2))
